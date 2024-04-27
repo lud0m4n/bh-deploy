0a4112956d20a6242ed3787f2e76f1d0c5cbfa0b
markgregr <markgrevtsov2310@gmail.com>
2024-04-27 19:45:44 +0000

    fix

--- cluster_chart/dash_app.py
+++ cluster_chart/dash_app.py
@@ -5,24 +5,30 @@ import pandas as pd
 import os
 from dotenv import load_dotenv 
 load_dotenv()
+
 # Загрузите данные для графиков
 elbow_data = pd.read_csv('data/elbow_data.csv')
 cluster_data = pd.read_csv('data/clustered_messages.csv')
 tsne_data = pd.read_csv('data/tsne_data.csv')
+time_data = pd.read_csv('data/output.csv')  # Загрузка данных средних метрик
+
 # Агрегация данных по кластеру и подсчет количества сообщений в каждом кластере
 cluster_counts = cluster_data['cluster'].value_counts().reset_index()
-cluster_counts.columns = ['cluster', 'count']  # Переименование столбцов для ясности
+cluster_counts.columns = ['cluster', 'count']
+
 # Создайте фигуры с использованием Plotly Express
 fig_elbow = px.line(elbow_data, x='Clusters', y='Inertia', title='Elbow Method For Optimal k', markers=True)
 fig_clusters = px.bar(cluster_counts, x='cluster', y='count', title='Frequency of Messages by Cluster')
 fig_tsne = px.scatter(tsne_data, x='t-SNE Feature 1', y='t-SNE Feature 2', color='cluster', title='t-SNE Visualization of Message Embeddings')
-
+fig_avg_duration = px.bar(time_data, x='ClusterIndex', y='AvgDuration', title='Average Duration by Cluster')
+fig_avg_reaction = px.bar(time_data, x='ClusterIndex', y='AvgReaction', title='Average Reaction by Cluster')
 
 graph_style = {
-    'width': '30%',        # Установка ширины графика
-    'display': 'inline-block', # Для расположения графиков в строке, если это нужно
-    'padding': '0 20px'    # Добавление отступов вокруг графика
+    'width': '30%',        
+    'display': 'inline-block',
+    'padding': '0 20px'
 }
+
 # Инициализация Dash приложения
 dash_app = dash.Dash(__name__)
 
@@ -50,9 +56,21 @@ dash_app.layout = html.Div(children=[
         id='tsne-visualization-graph',
         figure=fig_tsne,
         style=graph_style
+    ),
+    
+    dcc.Graph(
+        id='average-duration-graph',
+        figure=fig_avg_duration,
+        style=graph_style
+    ),
+
+    dcc.Graph(
+        id='average-reaction-graph',
+        figure=fig_avg_reaction,
+        style=graph_style
     )
 ])
 
 # Запуск сервера
 if __name__ == '__main__':
-    dash_app.run_server(host=os.getenv('PY_CHART_HOST'), debug=True, port=os.getenv('PY_CHART_PORT'))
+    dash_app.run_server(host=os.getenv('PY_CHART_HOST'), debug=True, port=int(os.getenv('PY_CHART_PORT')))
