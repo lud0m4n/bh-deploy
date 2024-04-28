f4d5ba95bc8e472f4826e34ed053741bf2226ee4
lud0m4n <fruktin03@gmail.com>
2024-04-28 08:21:35 +0000

    final #1

--- cluster_chart/dash_app.py
+++ cluster_chart/dash_app.py
@@ -6,7 +6,7 @@ import os
 from dotenv import load_dotenv 
 load_dotenv()
 
-# Загрузите данные для графиков
+# Загрузка данных для графиков
 elbow_data = pd.read_csv('data/elbow_data.csv')
 cluster_data = pd.read_csv('data/clustered_messages.csv')
 tsne_data = pd.read_csv('data/tsne_data.csv')
@@ -16,12 +16,14 @@ time_data = pd.read_csv('data/output.csv')  # Загрузка данных ср
 cluster_counts = cluster_data['cluster'].value_counts().reset_index()
 cluster_counts.columns = ['cluster', 'count']
 
-# Создайте фигуры с использованием Plotly Express
+# Фигуры с использованием Plotly Express
 fig_elbow = px.line(elbow_data, x='Clusters', y='Inertia', title='Elbow Method For Optimal k', markers=True)
 fig_clusters = px.bar(cluster_counts, x='cluster', y='count', title='Frequency of Messages by Cluster')
 fig_tsne = px.scatter(tsne_data, x='t-SNE Feature 1', y='t-SNE Feature 2', color='cluster', title='t-SNE Visualization of Message Embeddings')
 fig_avg_duration = px.bar(time_data, x='ClusterIndex', y='AvgDuration', title='Average Duration by Cluster')
 fig_avg_reaction = px.bar(time_data, x='ClusterIndex', y='AvgReaction', title='Average Reaction by Cluster')
+fig_median_duration = px.histogram(time_data, x='ClusterIndex', y='MedianDuration', title='Histogram of Median Durations by Cluster')
+fig_median_reaction = px.histogram(time_data, x='ClusterIndex', y='MedianReaction', title='Histogram of Median Reactions by Cluster')
 
 graph_style = {
     'width': '30%',        
@@ -72,6 +74,18 @@ dash_app.layout = html.Div(children=[
     dcc.Graph(
         id='average-reaction-graph',
         figure=fig_avg_reaction,
+        style=graph_style
+    ),
+
+    dcc.Graph(
+        id='median-duration-histogram',
+        figure=fig_median_duration,
+        style=graph_style
+    ),
+
+    dcc.Graph(
+        id='median-reaction-histogram',
+        figure=fig_median_reaction,
         style=graph_style
     )
 ])
